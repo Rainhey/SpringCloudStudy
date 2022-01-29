94ef80e7f21f81b00b22443d9a438c589f87533a
Rainhey <2190273698@qq.com>
2022-01-29 20:16:23 +0800

    "搭建生产者，消费者环境，练习RestTemplate"

---
+++ springcloud-api/src/main/java/com/rainhey/springcloud/pojo/Dept.java
@@ -0,0 +1,25 @@
+package com.rainhey.springcloud.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)  //链式写法
+public class Dept implements Serializable {
+    private long deptno;
+    private String dname;
+    private String db_resource;
+
+    public Dept(long deptno) {
+        this.deptno = deptno;
+    }
+
+    /*链式写法*/
+
+}
---
+++ springcloud-consumer-8000/src/main/java/com/rainhey/springcloud/controller/DeptConsumerController.java
@@ -0,0 +1,39 @@
+package com.rainhey.springcloud.controller;
+
+import com.rainhey.springcloud.pojo.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class DeptConsumerController {
+    //消费者不应该有service层
+    //RestTemplate供我们直接调用，注册到spring中
+    //RestTemplate 常用参数格式
+    /** 1. (url,Class<T> responseType)
+    * 2. (url,实体：map，Class<T> responseType*/
+
+    @Autowired
+    private RestTemplate restTemplate;  //提供多种边界访问http服务的方法，简单的restful模板
+
+    public static final String prefixurl="http://localhost:8001";
+
+    @RequestMapping("/consumer/dept/add")
+    public boolean add(Dept dept){
+        return restTemplate.postForObject(prefixurl+"/dept/add", dept, boolean.class);
+    }
+
+    @RequestMapping("/consumer/dept/get/{id}")
+    public Dept get(@PathVariable long id){
+        return restTemplate.getForObject(prefixurl+"/dept/get/"+id, Dept.class);
+    }
+
+    @RequestMapping("/consumer/dept/list")
+    public List<Dept> list(){
+        return restTemplate.getForObject(prefixurl+"/dept/list", List.class);
+    }
+}
